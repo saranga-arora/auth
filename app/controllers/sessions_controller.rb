d26aaebfdd779a2cd4fab4561f24fe544ccfc974
saranga-arora <saranga.arora@kellogg.northwestern.edu>
2022-02-22 16:04:51 +0000

    adds login experience

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+    def new 
+    end 
+
+    def create
+        entered_email = params["email"]
+        entered_password = params["password"] 
+        
+        #check if there is user with that email in database
+        @user = User.find_by({email: entered_email}) #expecting just one value
+        
+        #now, if email matches, check the password
+        if @user
+            if BCrypt::Password.new(@user.password) == entered_password 
+                flash[:notice] = "Welcome!"
+                redirect_to "/companies" #send to companies if password matches
+            else 
+                flash[:notice] = "Password is incorrect."
+                redirect_to "/sessions/new" #send back to login page if password doesn't match
+            end
+        else 
+            flash[:notice] = "No user with that email address."
+            redirect_to "/sessions/new" #send back to login page if no email in database
+        end 
+    end
+
+end
