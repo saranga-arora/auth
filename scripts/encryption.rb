3e15416cf4efee2bc71bd0b6a09ddc714ea1a58c
saranga-arora <saranga.arora@kellogg.northwestern.edu>
2022-02-22 15:31:21 +0000

    adds encryption code

--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -2,7 +2,12 @@
 # to test a secret, use BCrypt::Password.new(encrypted) == secret
 # run this file using rails runner scripts/encryption.rb
 
-# TODO:
 # 1. encrypt a secret and puts encrypted string
-# 2. decrypt the encrypted string
-# 3. check if decrypted value matches secret
+secret_password = "tacos"
+encrypted_password = BCrypt::Password.create(secret_password)
+puts encrypted_password 
+
+# 2. check if decrypted value matches secret
+password_to_test = "tacos"
+password_matches = BCrypt::Password.new(encrypted_password) == password_to_test
+puts password_matches
